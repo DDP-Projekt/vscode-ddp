bfedf2a403787c758abbb3accbe5e2b1fbe7111e
Hendrik Ziegler <hendrikziegler@outlook.de>
2022-08-29 10:21:05 +0200

    added a run-file command

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,19 @@
 
 import * as vscode from 'vscode';
 import * as langsrv from 'vscode-languageclient/node';
+import * as path from 'path';
+
+let DDPPATH = process.env.DDPPATH;
 
 export function activate(ctx: vscode.ExtensionContext) {
 	console.log('ddp extension activated');
+
+	let out = vscode.window.createOutputChannel("vscode-ddp", "ddp");
+	ctx.subscriptions.push(out);
+
+	if (DDPPATH === undefined || DDPPATH === "") {
+		out.appendLine("DDPPATH not found\nThis might cause errors in some commands");
+	}
 
 	// DDPLS must be installed and in the PATH
 	let serverOptions: langsrv.ServerOptions = {
@@ -22,16 +32,41 @@ export function activate(ctx: vscode.ExtensionContext) {
 
 	let lspClient = new langsrv.LanguageClient("ddpls", "DDPLS", serverOptions, clientOptions);
 
-	console.log("starting DDPLS");
 	let langSrvDisposable = lspClient.start();
 	ctx.subscriptions.push(langSrvDisposable);
 
 	ctx.subscriptions.push(vscode.commands.registerCommand("ddp.languageserver.restart", async () => {
-		console.log("restarting DDPLS");
+		out.appendLine("restarting DDPLS");
 		await lspClient.stop();
 		langSrvDisposable.dispose();
 		langSrvDisposable = lspClient.start();
 		ctx.subscriptions.push(langSrvDisposable);
+	}));
+
+	ctx.subscriptions.push(vscode.commands.registerCommand("ddp.run.file", async () => {
+		let currentFile = vscode.window.activeTextEditor?.document.fileName;
+		if (currentFile === undefined) { return; }
+		if (path.extname(currentFile) !== '.ddp') {
+			out.appendLine("file must be a .ddp file");
+			return;
+		}
+
+		if (DDPPATH === undefined) {
+			DDPPATH = "";
+		}
+
+		out.appendLine(currentFile);
+		let exe = path.join(DDPPATH, 'kddp.exe').replace(new RegExp('\\' + path.sep, 'g'), '/');
+		let filePath = currentFile.replace(new RegExp('\\' + path.sep, 'g'), '/');
+		let command = exe + " run " + filePath;
+
+		let terminal = vscode.window.activeTerminal;
+		if (terminal === undefined) {
+			terminal = vscode.window.createTerminal("ddp-run");
+			ctx.subscriptions.push(terminal);
+		}
+		terminal.show();
+		terminal.sendText(command);
 	}));
 }
 
